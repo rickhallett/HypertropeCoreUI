1cb8a152fdf6a7816ccb83b8bf1dafdf8d0876c5
Rick Hallett <rickhallett@live.co.uk>
2020-01-18 19:28:26 +0000

    jello animation on index card logo

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,22 +1,36 @@
 <template>
     <q-page class="flex ">
-        <q-card flat bordered class="my-card q-ma-md">
+        <q-card v-show="showCard" flat bordered class="card q-ma-md">
             <q-card-section>
-                <div class="text-h6">Welcome to Hypertrope Core!</div>
+                <div class="text-h6 text-center">Welcome to Hypertrope Core!</div>
             </q-card-section>
 
-            <div v-for="(quote, index) in quoteSelection" :key="index">
-                <quote-card :author="quote.author" :body="quote.body"></quote-card>
-            </div>
+            <q-card-section class="text-center">
+                <transition-group
+                    appear
+                    enter-active-class="animated fadeIn"
+                    leave-active-class="animated fadeOut"
+                >
+                    <div v-for="(quote, index) in quoteSelection" :key="index">
+                        <quote-card :author="quote.author" :body="quote.body"></quote-card>
+                    </div>
+                </transition-group>
+            </q-card-section>
 
-            <q-separator inset />
-
-            <div class="flex flex-center">
-                <q-btn dense flat size="132px" icon="donut_small" color="blue"/>
-            </div>
-
+            <transition appear
+                        enter-active-class="animated jello"
+                        leave-active-class="animated fadeOut">
+                <div v-show="showLogo" class="flex flex-center">
+                    <q-btn dense flat size="132px" icon="donut_small" color="blue"/>
+                </div>
+            </transition>
 
         </q-card>
+
+        <q-inner-loading :showing="loading">
+            <q-spinner-tail size="50px" color="primary" />
+        </q-inner-loading>
+
     </q-page>
 </template>
 
@@ -30,7 +44,10 @@
         },
         data() {
             return {
-                quotes: null
+                quotes: null,
+                loading: true,
+                showCard: false,
+                showLogo: false
             }
         },
         methods: {
@@ -50,15 +67,25 @@
             }
         },
         beforeCreate() {
-            this.$axios.get('https://localhost:5001/api/quote')
-                .then(res => {
-                    this.quotes = res.data.quotes
-                    console.log(this.quotes)
-                    console.log(this.quotes[0].author)
-                })
+            setTimeout(() => {
+                this.$axios.get('https://localhost:5001/api/quote')
+                    .then(res => {
+                        this.quotes = res.data.quotes
+                        this.loading = false
+                        this.showCard = true
+                        setTimeout(() => this.showLogo = true, 500)
+                    })
+            }, 1000)
+
         },
         created() {
 
         }
     }
 </script>
+
+<style>
+    .card {
+        min-width: 92%;
+    }
+</style>
